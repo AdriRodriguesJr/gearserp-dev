2bff116bbfc09bd5af0d01d8153e1625d5ffb2e6
Adriano <adriano.rodrigues.junior01@gmail.com>
2025-02-25 23:22:13 -0300

    ajustando as rotas do backend

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Rotas da API
-app.use('/api', Routes);
+app.use('desenvolvimento.gearserp.space/api', Routes);
 
 // Middleware de tratamento de erros
 app.use((err, req, res, next) => {
